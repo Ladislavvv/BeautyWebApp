44f7d82112c54d3c3be12bcb93442a87922d4933
Ladislavvv <zhukovdev@mail.ru>
2023-04-15 21:58:14 +0300

    full js without mm

--- js/script.js
+++ js/script.js
@@ -47,75 +47,200 @@ const cards = [
     fullDescription: "ТУТ ПОЛНОЕ ОПИСАНИЕ6",
     id: 6
   }
-
-
 ];
 
+const cardsKerastase = [
+  {
+    brand: "Nashi Argan",
+    price: 32340,
+    imgSrc: ["../img/items/Nashi Argan/1.png"],
+    description: 'Шампунь балансирующий для кожи головы ARMONIA',
+    fullDescription: "2222ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1ТУТ ПОЛНОЕ ОПИСАНИЕ1ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1ТУТ ПОЛНОЕ ОПИСАНИЕ1ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1ТУТ ПОЛНОЕ ОПИСАНИЕ1ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1ТУТ ПОЛНОЕ ОПИСАНИЕ1ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1ТУТ ПОЛНОЕ ОПИСАНИЕ1ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1ТУТ ПОЛНОЕ ОПИСАНИЕ1ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1ТУТ ПОЛНОЕ ОПИСАНИЕ1",
+    id: 1
+  },
+  {
+    brand: "Kerastase",
+    price: 42400,
+    imgSrc: ["../img/items/Nashi Argan/1.png", "../img/items/Nashi Argan/2.png"],
+    description: 'Шампунь Nutritive для сухих волос',
+    fullDescription: "222ТУТ ПОЛНОЕ ОПИСАНИЕ2 ТУТ ПОЛНОЕ ОПИСАНИЕ2 ТУТ ПОЛНОЕ ОПИСАНИЕ2 ТУТ ПОЛНОЕ ОПИСАНИЕ2",
+    id: 2
+  },
+  {
+    brand: "L'Oreal Professionnel",
+    price: 21500,
+    imgSrc: ["../img/items/Nashi Argan/1.png"],
+    description: "Шампунь Vitamino Color для окрашенных волос",
+    fullDescription: "222ТУТ ПОЛНОЕ ОПИСАНИЕ3",
+    id: 3
+  },
+  {
+    brand: "L'Oreal Professionnel",
+    price: 14500,
+    imgSrc: ["../img/items/Nashi Argan/1.png", "../img/items/Nashi Argan/2.png", "../img/items/Nashi Argan/2.png"],
+    description: "Шампунь Vitamino Color для окрашенных волос",
+    fullDescription: "222ТУТ ПОЛНОЕ ОПИСАНИЕ4",
+    id: 4
+  },
+  {
+    brand: "L'Oreal Professionnel",
+    price: 11500,
+    imgSrc: ["../img/items/Nashi Argan/1.png", "../img/items/Nashi Argan/2.png", "../img/items/Nashi Argan/2.png", "../img/items/Nashi Argan/2.png", "../img/items/Nashi Argan/2.png"],
+    description: "Шампунь Vitamino Color для окрашенных волос",
+    fullDescription: "222ТУТ ПОЛНОЕ ОПИСАНИЕ5",
+    id: 5
+  },
+  {
+    brand: "L'Oreal Professionnel",
+    price: 15500,
+    imgSrc: ["../img/items/Nashi Argan/1.png"],
+    description: "Шампунь Vitamino ыпыпыпColor для окрашенных волос",
+    fullDescription: "222ТУТ ПОЛНОЕ ОПИСАНИЕ6",
+    id: 6
+  }
+];
+
+
+
 const brandCards = document.querySelector('.brand__cards');
+const brandCards2 = document.querySelector('.brand__cards2');
 
 
-// проходим по каждому объекту в массиве cards
-cards.forEach(card => {
-  // создаем новый элемент карточки
-  const newCard = document.createElement('div');
-  newCard.classList.add('brand__card');
-  newCard.id = card.id;
-  newCard.setAttribute('onclick', 'openPopup(id)');
-  // newCard
+renderCards(cards);
+renderCards(cardsKerastase);
 
-  // добавляем в него изображение
-  const cardImg = document.createElement('div');
-  cardImg.classList.add('card__img');
-  const img = document.createElement('img');
-  img.setAttribute('src', card.imgSrc[0]);
-  img.setAttribute('alt', '');
-  cardImg.appendChild(img);
-  newCard.appendChild(cardImg);
+function renderCards(typeMass) {
 
-  // добавляем в него цену
-  const cardPrice = document.createElement('div');
-  cardPrice.classList.add('card__price');
-  const price = document.createElement('span');
-  price.innerText = card.price;
-  cardPrice.appendChild(price);
-  const currency = document.createElement('span');
-  currency.innerText = '₽';
-  cardPrice.appendChild(currency);
-  newCard.appendChild(cardPrice);
+  // проходим по каждому объекту в массиве cards
+  typeMass.forEach(card => {
+    // создаем новый элемент карточки
+    const newCard = document.createElement('div');
+    // let dataBrand;
+    let dataCards;
+    if (typeMass == cards) {
+      // dataBrand = 'brand__card';
+      dataCards = document.querySelector('.brand__cards');;
+      newCard.setAttribute('onclick', 'openPopup(id)');
+    }
+    if (typeMass == cardsKerastase) {
+      // dataBrand = 'brand__card';
+      dataCards = document.querySelector('.brand__cards2');
+      newCard.setAttribute('onclick', 'openPopup2(id)');
+    }
+    newCard.classList.add('brand__card');
+    newCard.id = card.id;
+    // newCard.setAttribute('onclick', 'openPopup(id)');
+    // newCard
 
-  // добавляем в него бренд
-  const brand = document.createElement('p');
-  brand.classList.add('card__brand');
-  brand.innerText = card.brand;
-  newCard.appendChild(brand);
+    // добавляем в него изображение
+    const cardImg = document.createElement('div');
+    cardImg.classList.add('card__img');
+    const img = document.createElement('img');
+    img.setAttribute('src', card.imgSrc[0]);
+    img.setAttribute('alt', '');
+    cardImg.appendChild(img);
+    newCard.appendChild(cardImg);
 
-  // добавляем в него описание
-  const description = document.createElement('p');
-  description.classList.add('card__about');
-  description.innerText = card.description;
-  newCard.appendChild(description);
+    // добавляем в него цену
+    const cardPrice = document.createElement('div');
+    cardPrice.classList.add('card__price');
+    const price = document.createElement('span');
+    price.innerText = card.price;
+    cardPrice.appendChild(price);
+    const currency = document.createElement('span');
+    currency.innerText = '₽';
+    cardPrice.appendChild(currency);
+    newCard.appendChild(cardPrice);
 
-  // добавляем новую карточку в контейнер
-  brandCards.appendChild(newCard);
+    // добавляем в него бренд
+    const brand = document.createElement('p');
+    brand.classList.add('card__brand');
+    brand.innerText = card.brand;
+    newCard.appendChild(brand);
 
-});
+    // добавляем в него описание
+    const description = document.createElement('p');
+    description.classList.add('card__about');
+    description.innerText = card.description;
+    newCard.appendChild(description);
+
+    // добавляем новую карточку в контейнер
+    dataCards.appendChild(newCard);
+
+  });
+}
+
+// // проходим по каждому объекту в массиве cards
+// cards.forEach(card => {
+//   // создаем новый элемент карточки
+//   const newCard = document.createElement('div');
+//   newCard.classList.add('brand__card');
+//   newCard.id = card.id;
+//   newCard.setAttribute('onclick', 'openPopup(id)');
+//   // newCard
+
+//   // добавляем в него изображение
+//   const cardImg = document.createElement('div');
+//   cardImg.classList.add('card__img');
+//   const img = document.createElement('img');
+//   img.setAttribute('src', card.imgSrc[0]);
+//   img.setAttribute('alt', '');
+//   cardImg.appendChild(img);
+//   newCard.appendChild(cardImg);
+
+//   // добавляем в него цену
+//   const cardPrice = document.createElement('div');
+//   cardPrice.classList.add('card__price');
+//   const price = document.createElement('span');
+//   price.innerText = card.price;
+//   cardPrice.appendChild(price);
+//   const currency = document.createElement('span');
+//   currency.innerText = '₽';
+//   cardPrice.appendChild(currency);
+//   newCard.appendChild(cardPrice);
+
+//   // добавляем в него бренд
+//   const brand = document.createElement('p');
+//   brand.classList.add('card__brand');
+//   brand.innerText = card.brand;
+//   newCard.appendChild(brand);
+
+//   // добавляем в него описание
+//   const description = document.createElement('p');
+//   description.classList.add('card__about');
+//   description.innerText = card.description;
+//   newCard.appendChild(description);
+
+//   // добавляем новую карточку в контейнер
+//   brandCards.appendChild(newCard);
+
+// });
 
 
 
 const overlay = document.querySelector('.overlay');
 const popup = document.querySelector('.popup');
 const popup__container = document.querySelector('.popup-container');
-
+const bodyEl = document.querySelector('body');
 
 function openPopup(id) {
   popup.classList.add('active');
+  bodyEl.classList.add('popup-open');
   // overlay.classList.add('active');
 
   updatePopup(id);
 }
 
+function openPopup2(id) {
+  popup.classList.add('active');
+  bodyEl.classList.add('popup-open');
+
+  updatePopup2(id);
+}
+
 function closePopup() {
   popup.classList.remove('active');
+  bodyEl.classList.remove('popup-open');
   // overlay.classList.remove('active');
 }
 
@@ -131,7 +256,7 @@ overlay.addEventListener('click', function (event) {
 
 // Актуализация инфы в модалке
 
-const popupTitle = document.querySelector('.title');
+// const popupTitle = document.querySelector('.title');
 const popupBrand = document.querySelector('.description__brand');
 const popupName = document.querySelector('.description__name');
 const popupAbout = document.querySelector('.description__about');
@@ -144,15 +269,12 @@ function updatePopup(num) {
   // let data = cards.find((card) => card.id == num){
   //   popupTitle.innerHTML
   // }
-  allCards.forEach(card => {
+
+  cards.forEach(card => {
     if (card.id == num) {
       // const newPrice = card.querySelector('card-price');
       const cardId = num - 1;
 
-      // console.log(popupName.innerHTML);
-      // console.log(cards[cardId].description);
-
-      popupTitle.innerHTML = cards[cardId].brand;
       popupBrand.innerHTML = cards[cardId].brand;
       popupName.innerHTML = cards[cardId].description;
       popupAbout.innerHTML = cards[cardId].fullDescription;
@@ -161,9 +283,45 @@ function updatePopup(num) {
     }
   })
   // let cardNum = cards.find((card) => card.id == num);
-
   updateSlides(num - 1);
 };
+
+function updatePopup2(num) {
+  cardsKerastase.forEach(card => {
+    if (card.id == num) {
+      // const newPrice = card.querySelector('card-price');
+      const cardId = num - 1;
+
+      popupBrand.innerHTML = cardsKerastase[cardId].brand;
+      popupName.innerHTML = cardsKerastase[cardId].description;
+      popupAbout.innerHTML = cardsKerastase[cardId].fullDescription;
+      popupPrice.innerHTML = cardsKerastase[cardId].price;
+
+    }
+  })
+  // let cardNum = cards.find((card) => card.id == num);
+  updateSlides2(num - 1);
+}
+
+
+//
+// function updatePopup(num) {
+//   // let data = cards.find((card) => card.id == num){
+//   allCards.forEach(card => {
+//     if (card.id == num) {
+//       const cardId = num - 1;
+//       popupBrand.innerHTML = cards[cardId].brand;
+//       popupName.innerHTML = cards[cardId].description;
+//       popupAbout.innerHTML = cards[cardId].fullDescription;
+//       popupPrice.innerHTML = cards[cardId].price;
+
+//     }
+//   })
+//   // let cardNum = cards.find((card) => card.id == num);
+
+//   updateSlides(num - 1);
+// };
+// 
 
 // Карусель настройка
 
@@ -205,11 +363,40 @@ function updateSlides(cardNumber) {
     leftArrow123.classList.add('hide');
     rightArrow123.classList.add('hide');
   }
-
 }
-{/* <div class="carousel-item active">
-    <img src="../img/items/Nashi Argan/luxancee1.png" class="d-block w-100" alt="...">
-</div> */}
 
-// const newCard = document.createElement('div');
-// newCard.classList.add('brand__card');
+// 2 карусель
+
+function updateSlides2(cardNumber) {
+  carouselInner.innerHTML = '';
+  let dataCatalog = cardsKerastase;
+  const newCarouselItemActive = document.createElement('div');
+  newCarouselItemActive.classList.add('carousel-item');
+  newCarouselItemActive.classList.add('active');
+
+  const newImgA = document.createElement('img');
+  newImgA.setAttribute('class', 'd-block w-100');
+  newImgA.setAttribute('alt', '...');
+  newImgA.setAttribute('src', dataCatalog[cardNumber].imgSrc[0]);
+  newCarouselItemActive.appendChild(newImgA);
+  carouselInner.appendChild(newCarouselItemActive);
+
+  if (dataCatalog[cardNumber].imgSrc.length > 1) {
+    leftArrow123.classList.remove('hide');
+    rightArrow123.classList.remove('hide');
+
+    for (let i = 1; i < dataCatalog[cardNumber].imgSrc.length; i++) {
+      const newCarouselItem = document.createElement('div');
+      newCarouselItem.classList.add('carousel-item');
+      const newImg = document.createElement('img');
+      newImg.setAttribute('class', 'd-block w-100');
+      newImg.setAttribute('alt', '...');
+      newImg.setAttribute('src', dataCatalog[cardNumber].imgSrc[i]);
+      newCarouselItem.appendChild(newImg);
+      carouselInner.appendChild(newCarouselItem);
+    }
+  } else {
+    leftArrow123.classList.add('hide');
+    rightArrow123.classList.add('hide');
+  }
+}
