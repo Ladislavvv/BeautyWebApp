ef70764417fd5e10307689ae0093af09d55c90f2
Ladislavvv <zhukovdev@mail.ru>
2023-04-16 13:03:16 +0300

    full js

--- js/new.js
+++ js/new.js
@@ -1,38 +1,317 @@
 const all_Cards = [
     [
         {
-            brand: "Nashi Argan",
-            price: 3240,
+            brand: "111Nashi Argan",
+            price: 111,
             imgSrc: ["../img/items/Nashi Argan/1.png"],
-            description: 'Шампунь балансирующий для кожи головы ARMONIA',
-            fullDescription: "ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1ТУТ ПОЛНОЕ ОПИСАНИЕ1ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1ТУТ ПОЛНОЕ ОПИСАНИЕ1ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1ТУТ ПОЛНОЕ ОПИСАНИЕ1ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1ТУТ ПОЛНОЕ ОПИСАНИЕ1ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1ТУТ ПОЛНОЕ ОПИСАНИЕ1ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1ТУТ ПОЛНОЕ ОПИСАНИЕ1ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1ТУТ ПОЛНОЕ ОПИСАНИЕ1",
+            description: '1Шампунь балансирующий для кожи головы ARMONIA',
+            fullDescription: "1ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1ТУТ ПОЛНОЕ ОПИСАНИЕ1ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1ТУТ ПОЛНОЕ ОПИСАНИЕ1ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1ТУТ ПОЛНОЕ ОПИСАНИЕ1ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1ТУТ ПОЛНОЕ ОПИСАНИЕ1ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1ТУТ ПОЛНОЕ ОПИСАНИЕ1ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1ТУТ ПОЛНОЕ ОПИСАНИЕ1ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1ТУТ ПОЛНОЕ ОПИСАНИЕ1",
             id: 1
         },
         {
-            brand: "Kerastase",
-            price: 4200,
-            imgSrc: ["../img/items/Nashi Argan/2.png", "../img/items/Nashi Argan/2.png"],
-            description: 'Шампунь Nutritive для сухих волос',
-            fullDescription: "ТУТ ПОЛНОЕ ОПИСАНИЕ2 ТУТ ПОЛНОЕ ОПИСАНИЕ2 ТУТ ПОЛНОЕ ОПИСАНИЕ2 ТУТ ПОЛНОЕ ОПИСАНИЕ2",
+            brand: "112Kerastase",
+            price: 1112,
+            imgSrc: ["../img/items/Nashi Argan/2.png"],
+            description: '1Шампунь Nutritive для сухих волос',
+            fullDescription: "1ТУТ ПОЛНОЕ ОПИСАНИЕ2 ТУТ ПОЛНОЕ ОПИСАНИЕ2 ТУТ ПОЛНОЕ ОПИСАНИЕ2 ТУТ ПОЛНОЕ ОПИСАНИЕ2",
             id: 2
         }
     ],
     [
         {
-            brand: "Nashi Argan",
-            price: 3240,
+            brand: "211Nashi Argan",
+            price: 2222,
             imgSrc: ["../img/items/Nashi Argan/1.png"],
-            description: 'Шампунь балансирующий для кожи головы ARMONIA',
-            fullDescription: "ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1ТУТ ПОЛНОЕ ОПИСАНИЕ1ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1ТУТ ПОЛНОЕ ОПИСАНИЕ1ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1ТУТ ПОЛНОЕ ОПИСАНИЕ1ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1ТУТ ПОЛНОЕ ОПИСАНИЕ1ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1ТУТ ПОЛНОЕ ОПИСАНИЕ1ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1ТУТ ПОЛНОЕ ОПИСАНИЕ1ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1ТУТ ПОЛНОЕ ОПИСАНИЕ1",
+            description: '2Шампунь балансирующий для кожи головы ARMONIA',
+            fullDescription: "2ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1ТУТ ПОЛНОЕ ОПИСАНИЕ1ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1ТУТ ПОЛНОЕ ОПИСАНИЕ1ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1ТУТ ПОЛНОЕ ОПИСАНИЕ1ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1ТУТ ПОЛНОЕ ОПИСАНИЕ1ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1ТУТ ПОЛНОЕ ОПИСАНИЕ1ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1ТУТ ПОЛНОЕ ОПИСАНИЕ1ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1ТУТ ПОЛНОЕ ОПИСАНИЕ1",
             id: 1
         },
         {
-            brand: "Kerastase",
-            price: 4200,
+            brand: "212Kerastase",
+            price: 2224,
             imgSrc: ["../img/items/Nashi Argan/2.png", "../img/items/Nashi Argan/2.png"],
-            description: 'Шампунь Nutritive для сухих волос',
-            fullDescription: "ТУТ ПОЛНОЕ ОПИСАНИЕ2 ТУТ ПОЛНОЕ ОПИСАНИЕ2 ТУТ ПОЛНОЕ ОПИСАНИЕ2 ТУТ ПОЛНОЕ ОПИСАНИЕ2",
+            description: '2Шампунь Nutritive для сухих волос',
+            fullDescription: "2ТУТ ПОЛНОЕ ОПИСАНИЕ2 ТУТ ПОЛНОЕ ОПИСАНИЕ2 ТУТ ПОЛНОЕ ОПИСАНИЕ2 ТУТ ПОЛНОЕ ОПИСАНИЕ2",
+            id: 2
+        }
+    ],
+    [
+        {
+            brand: "311Nashi Argan",
+            price: 3333,
+            imgSrc: ["../img/items/Nashi Argan/1.png"],
+            description: '3Шампунь балансирующий для кожи головы ARMONIA',
+            fullDescription: "3ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1ТУТ ПОЛНОЕ ОПИСАНИЕ1ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1ТУТ ПОЛНОЕ ОПИСАНИЕ1ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1ТУТ ПОЛНОЕ ОПИСАНИЕ1ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1ТУТ ПОЛНОЕ ОПИСАНИЕ1ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1ТУТ ПОЛНОЕ ОПИСАНИЕ1ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1ТУТ ПОЛНОЕ ОПИСАНИЕ1ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1 ТУТ ПОЛНОЕ ОПИСАНИЕ1ТУТ ПОЛНОЕ ОПИСАНИЕ1",
+            id: 1
+        },
+        {
+            brand: "312Kerastase",
+            price: 3334,
+            imgSrc: ["../img/items/Nashi Argan/2.png", "../img/items/Nashi Argan/2.png"],
+            description: '3Шампунь Nutritive для сухих волос',
+            fullDescription: "3ТУТ ПОЛНОЕ ОПИСАНИЕ2 ТУТ ПОЛНОЕ ОПИСАНИЕ2 ТУТ ПОЛНОЕ ОПИСАНИЕ2 ТУТ ПОЛНОЕ ОПИСАНИЕ2",
             id: 2
         }
     ]
 ]
+
+
+// 
+const overlay = document.querySelector('.overlay');
+const popup = document.querySelector('.popup');
+const popup__container = document.querySelector('.popup-container');
+const bodyEl = document.querySelector('body');
+// 
+
+// 
+const popupBrand = document.querySelector('.description__brand');
+const popupName = document.querySelector('.description__name');
+const popupAbout = document.querySelector('.description__about');
+const popupPrice = document.querySelector('.priceRub');
+// 
+
+// 
+// Карусель настройка
+
+const carouselInner = document.querySelector('.carousel-inner');
+const carouselIndicators = document.querySelector('.carousel-indicators');
+
+const leftArrow123 = document.querySelector('.carousel-control-prev');
+const rightArrow123 = document.querySelector('.carousel-control-next');
+// 
+
+// 
+
+const brand_Сards = document.querySelectorAll('.brand__cards');
+const brand_СardsMass = [];
+brand_Сards.forEach((element) => {
+    brand_СardsMass.push(element)
+});
+
+// 
+
+
+for (let k = 0; k < brand_СardsMass.length; k++) {
+    const dataMass = all_Cards[k];
+    const dataBrandCard = brand_СardsMass[k];
+    renderCards(dataMass, dataBrandCard)
+}
+
+// const brandCards = document.querySelector('.brand__cards');
+
+
+function renderCards(typeMass, dataCards) {
+
+
+    // проходим по каждому объекту в массиве cards
+    typeMass.forEach(card => {
+        // создаем новый элемент карточки
+        const newCard = document.createElement('div');
+
+        // if (typeMass == 0) {
+        //     newCard.setAttribute('onclick', 'openPopup(id)');
+        // }
+        // if (typeMass == 1) {
+        //     newCard.setAttribute('onclick', 'openPopup2(id)');
+        // }
+        newCard.classList.add('brand__card');
+        newCard.id = card.id;
+        newCard.setAttribute('onclick', 'openPopup(id, this)');
+        // newCard
+
+        // добавляем в него изображение
+        const cardImg = document.createElement('div');
+        cardImg.classList.add('card__img');
+        const img = document.createElement('img');
+        img.setAttribute('src', card.imgSrc[0]);
+        img.setAttribute('alt', '');
+        cardImg.appendChild(img);
+        newCard.appendChild(cardImg);
+
+        // добавляем в него цену
+        const cardPrice = document.createElement('div');
+        cardPrice.classList.add('card__price');
+        const price = document.createElement('span');
+        price.innerText = card.price;
+        cardPrice.appendChild(price);
+        const currency = document.createElement('span');
+        currency.innerText = '₽';
+        cardPrice.appendChild(currency);
+        newCard.appendChild(cardPrice);
+
+        // добавляем в него бренд
+        const brand = document.createElement('p');
+        brand.classList.add('card__brand');
+        brand.innerText = card.brand;
+        newCard.appendChild(brand);
+
+        // добавляем в него описание
+        const description = document.createElement('p');
+        description.classList.add('card__about');
+        description.innerText = card.description;
+        newCard.appendChild(description);
+
+        // добавляем новую карточку в контейнер
+        dataCards.appendChild(newCard);
+
+    });
+}
+
+
+// const overlay = document.querySelector('.overlay');
+// const popup = document.querySelector('.popup');
+// const popup__container = document.querySelector('.popup-container');
+// const bodyEl = document.querySelector('body');
+
+function openPopup(id, hero) {
+    // const popup = document.querySelector('.popup');
+    // const bodyEl = document.querySelector('body');
+    popup.classList.add('active');
+    bodyEl.classList.add('popup-open');
+
+    updatePopup(id, hero);
+}
+
+
+function closePopup() {
+    // const popup = document.querySelector('.popup');
+    // const bodyEl = document.querySelector('body');
+    popup.classList.remove('active');
+    bodyEl.classList.remove('popup-open');
+    // overlay.classList.remove('active');
+}
+
+
+overlay.addEventListener('click', function (event) {
+    const popup__container = document.querySelector('.popup-container');
+    const isClickInsidePopup = popup__container.contains(event.target);
+    // console.log(isClickInsidePopup);
+    if (!isClickInsidePopup) {
+        closePopup();
+    }
+});
+
+
+
+// Актуализация инфы в модалке
+
+// const popupTitle = document.querySelector('.title');
+// const popupBrand = document.querySelector('.description__brand');
+// const popupName = document.querySelector('.description__name');
+// const popupAbout = document.querySelector('.description__about');
+// const popupPrice = document.querySelector('.priceRub');
+
+const allCards = document.querySelectorAll('.brand__card');
+
+
+function updatePopup(num, hero) {
+    // let data = cards.find((card) => card.id == num){
+    //   popupTitle.innerHTML
+    // }
+    const paretnBrandcards = hero.parentNode;
+    // console.log(paretnBrandcards);
+
+    let dataIdCard;
+    for (let p = 0; p < brand_СardsMass.length; p++) {
+        if (paretnBrandcards == brand_СardsMass[p]) {
+            dataIdCard = p;
+            // console.log(dataIdCard);
+        }
+    }
+    all_Cards[dataIdCard].forEach(card => {
+        if (card.id == num) {
+            // const newPrice = card.querySelector('card-price');
+            const cardId = num - 1;
+
+            popupBrand.innerHTML = all_Cards[dataIdCard][cardId].brand;
+            popupName.innerHTML = all_Cards[dataIdCard][cardId].description;
+            popupAbout.innerHTML = all_Cards[dataIdCard][cardId].fullDescription;
+            popupPrice.innerHTML = all_Cards[dataIdCard][cardId].price;
+
+        }
+    })
+    // let cardNum = cards.find((card) => card.id == num);
+    // updateSlides(num - 1, brand_СardsMass[p]);
+    updateSlides(num - 1, all_Cards[dataIdCard]);
+};
+
+
+
+
+// Карусель настройка
+
+// const carouselInner = document.querySelector('.carousel-inner');
+// const carouselIndicators = document.querySelector('.carousel-indicators');
+
+// const leftArrow123 = document.querySelector('.carousel-control-prev');
+// const rightArrow123 = document.querySelector('.carousel-control-next');
+
+
+function updateSlides(cardNumber, dataMasss) {
+    carouselInner.innerHTML = '';
+
+    const newCarouselItemActive = document.createElement('div');
+    newCarouselItemActive.classList.add('carousel-item');
+    newCarouselItemActive.classList.add('active');
+
+    const newImgA = document.createElement('img');
+    newImgA.setAttribute('class', 'd-block w-100');
+    newImgA.setAttribute('alt', '...');
+    newImgA.setAttribute('src', dataMasss[cardNumber].imgSrc[0]);
+    newCarouselItemActive.appendChild(newImgA);
+    carouselInner.appendChild(newCarouselItemActive);
+
+    if (dataMasss[cardNumber].imgSrc.length > 1) {
+        leftArrow123.classList.remove('hide');
+        rightArrow123.classList.remove('hide');
+
+        for (let i = 1; i < dataMasss[cardNumber].imgSrc.length; i++) {
+            const newCarouselItem = document.createElement('div');
+            newCarouselItem.classList.add('carousel-item');
+            const newImg = document.createElement('img');
+            newImg.setAttribute('class', 'd-block w-100');
+            newImg.setAttribute('alt', '...');
+            newImg.setAttribute('src', dataMasss[cardNumber].imgSrc[i]);
+            newCarouselItem.appendChild(newImg);
+            carouselInner.appendChild(newCarouselItem);
+        }
+    } else {
+        leftArrow123.classList.add('hide');
+        rightArrow123.classList.add('hide');
+    }
+}
+
+// function updateSlides(cardNumber, dataMasss) {
+//     carouselInner.innerHTML = '';
+
+//     const newCarouselItemActive = document.createElement('div');
+//     newCarouselItemActive.classList.add('carousel-item');
+//     newCarouselItemActive.classList.add('active');
+
+//     const newImgA = document.createElement('img');
+//     newImgA.setAttribute('class', 'd-block w-100');
+//     newImgA.setAttribute('alt', '...');
+//     newImgA.setAttribute('src', dataMasss[cardNumber].imgSrc[0]);
+//     newCarouselItemActive.appendChild(newImgA);
+//     carouselInner.appendChild(newCarouselItemActive);
+
+//     if (dataMasss[cardNumber].imgSrc.length > 1) {
+//         leftArrow123.classList.remove('hide');
+//         rightArrow123.classList.remove('hide');
+
+//         for (let i = 1; i < dataMasss[cardNumber].imgSrc.length; i++) {
+//             const newCarouselItem = document.createElement('div');
+//             newCarouselItem.classList.add('carousel-item');
+//             const newImg = document.createElement('img');
+//             newImg.setAttribute('class', 'd-block w-100');
+//             newImg.setAttribute('alt', '...');
+//             newImg.setAttribute('src', dataMasss[cardNumber].imgSrc[i]);
+//             newCarouselItem.appendChild(newImg);
+//             carouselInner.appendChild(newCarouselItem);
+//         }
+//     } else {
+//         leftArrow123.classList.add('hide');
+//         rightArrow123.classList.add('hide');
+//     }
+// }
+
